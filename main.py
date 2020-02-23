93cb5231755543b9262d8184a35c279e0f5e5631
Marco Bellido <marco.bellido@letsgoi.com>
2020-02-23 09:40:52 +0000

    Fixed typo (local tests...)

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from TwitterPublisher import TwitterPublisher
 dotenv.load_dotenv()
 
 if __name__ == '__main__':
-    tweet = input('Enter yourasd tweet: ')
+    tweet = input('Enter your tweet: ')
 
     twitter_publisher = TwitterPublisher()
     twitter_publisher.publish(tweet)
